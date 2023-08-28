59e7aa64bd26a7fb39bdd7fa9f4bc1b13348f1a6
Chetan <ckalia@uwaterloo.ca>
2023-08-27 22:08:27 -0400

    added user model and createUser mutation and relationship between event and user

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-require('dotenv').config()
+require('dotenv').config();
 
 const Event = require('./models/event');
+const User = require('./models/user');
 
 const app = express();
 
@@ -24,11 +26,22 @@ app.use(
           date: String!
         }
 
+        type User {
+          _id: ID!,
+          email: String!,
+          password: String
+        }
+
         input EventInput {
           title: String!
           description: String!
           price: Float!
           date: String!
+        }
+
+        input UserInput {
+          email: String!,
+          password: String!,
         }
 
         type RootQuery {
@@ -37,6 +50,7 @@ app.use(
 
         type RootMutation {
             createEvent(eventInput: EventInput): Event
+            createUser(userInput: UserInput): User
         }
 
         schema {
@@ -47,35 +61,77 @@ app.use(
     rootValue: {
       events: () => {
         return Event.find()
-          .then(events => {
-            return events.map(event => {
+          .then((events) => {
+            return events.map((event) => {
               return { ...event._doc, _id: event.id };
             });
           })
-          .catch(err => {
+          .catch((err) => {
             throw err;
           });
       },
-      createEvent: args => {
+      createEvent: (args) => {
         const event = new Event({
           title: args.eventInput.title,
           description: args.eventInput.description,
           price: +args.eventInput.price,
-          date: new Date(args.eventInput.date)
+          date: new Date(args.eventInput.date),
+          creator: '64ebf62af06abf5d5483eb41',
         });
+        let createdEvent;
         return event
           .save()
-          .then(result => {
-            console.log(result);
-            return { ...result._doc, _id: result._doc._id.toString() };
+          .then((result) => {
+            createdEvent = { ...result._doc, _id: result._doc._id.toString() };
+            return User.findById('64ebf62af06abf5d5483eb41');
           })
-          .catch(err => {
+          .then((user) => {
+            if (!user) {
+              throw new Error('user not found');
+            }
+            user.createdEvents.push(event);
+            return user.save();
+          })
+          .then((_) => {
+            return createdEvent;
+          })
+          .catch((err) => {
+            throw err;
+          });
+      },
+      createUser: (args) => {
+        return User.findOne({ email: args.userInput.email })
+          .then((res) => {
+            if (res) {
+              throw new Error('User already exists');
+            }
+            const saltRounds = 10;
+            return bcrypt.genSalt(saltRounds);
+          })
+          .then((salt) => {
+            return bcrypt.hash(args.userInput.password, salt);
+          })
+          .then((hash) => {
+            const user = new User({
+              email: args.userInput.email,
+              password: hash,
+            });
+            return user.save();
+          })
+          .then((result) => {
+            return {
+              ...result._doc,
+              password: null,
+              _id: result._doc._id.toString(),
+            };
+          })
+          .catch((err) => {
             console.log(err);
             throw err;
           });
-      }
+      },
     },
-    graphiql: true
+    graphiql: true,
   })
 );
 
@@ -84,12 +140,12 @@ mongoose
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.vantgos.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
     {
       useNewUrlParser: true,
-      useUnifiedTopology: true 
+      useUnifiedTopology: true,
     }
   )
   .then(() => {
     app.listen(3000);
   })
-  .catch(err => {
+  .catch((err) => {
     console.log(err);
   });
