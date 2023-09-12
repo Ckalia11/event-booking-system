80c4e43d3a15939ced7ec5db873984d089d5e07d
Chetan <ckalia@uwaterloo.ca>
2023-09-12 13:51:38 -0400

    added formik add event form

--- frontend/src/pages/addEvent.js
+++ frontend/src/pages/addEvent.js
@@ -1,78 +1,160 @@
 import React, { useState, useEffect, useRef, useContext } from 'react';
 import AuthContext from '../context/authContext';
 import NavigationBar from '../components/nav';
+import { Box, TextField, Button } from '@mui/material';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import dayjs from 'dayjs';
+
+const validationSchema = yup.object({
+  title: yup.string('Enter the title').required('Email is required'),
+  description: yup
+    .string('Enter the description')
+    .required('Description is required'),
+  price: yup.number('Enter the price').required('Price is required'),
+  date: yup.date('Enter the date').required('Date is required'),
+});
 
 export default function AddEvent() {
   const { authInfo } = useContext(AuthContext);
   const { token } = authInfo;
 
-  const titleEl = useRef();
-  const descriptionEl = useRef();
-  const priceEl = useRef();
-  const dateEl = useRef();
+  const blockInvalidChar = (e) =>
+    ['e', 'E', '+', '-'].includes(e.key) && e.preventDefault();
 
-  const submitHandler = (e) => {
-    e.preventDefault();
+  const formik = useFormik({
+    initialValues: {
+      title: '',
+      description: '',
+      price: '',
+      date: null,
+    },
+    validationSchema,
+    onSubmit: (values) => {
+      const title = values.title;
+      const description = values.description;
+      const price = values.price;
+      const date = values.date.toDate();
 
-    const title = titleEl.current.value;
-    const description = descriptionEl.current.value;
-    const price = priceEl.current.value;
-    const date = dateEl.current.value;
-
-    const uri = 'http://localhost:9000/graphql';
-    const requestBody = {
-      query: `mutation {
-      createEvent(eventInput: {title: "${title}" description: "${description}" price: ${price} date: "${date}"}) {
+      const uri = 'http://localhost:9000/graphql';
+      const requestBody = {
+        query: `mutation {
+      createEvent(eventInput: {title: "${title}" description: "${description}" price: ${price} date: "${date.toISOString()}"}) {
         title
         description
         price
         date
       }
     }`,
-    };
-    fetch(uri, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: 'Bearer ' + token,
-      },
-      body: JSON.stringify(requestBody),
-    })
-      .then((response) => {
-        return response.json();
+      };
+      fetch(uri, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + token,
+        },
+        body: JSON.stringify(requestBody),
       })
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  });
 
   return (
     <React.Fragment>
       <NavigationBar />
-      <form method="POST" onSubmit={submitHandler}>
+      <Box
+        component="form"
+        onSubmit={formik.handleSubmit}
+        sx={{
+          '& .MuiTextField-root': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
         <div>
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" ref={titleEl}></input>
+          <TextField
+            required
+            id="title"
+            label="Title"
+            title="title"
+            autoComplete="title"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.title}
+            error={formik.touched.title && Boolean(formik.errors.title)}
+            helperText={formik.touched.title && formik.errors.title}
+          />
         </div>
         <div>
-          <label htmlFor="description">Description</label>
-          <input type="text" id="description" ref={descriptionEl}></input>
+          <TextField
+            required
+            id="description"
+            label="Description"
+            title="description"
+            multiline
+            rows={2}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.description}
+            error={
+              formik.touched.description && Boolean(formik.errors.description)
+            }
+            helperText={formik.touched.description && formik.errors.description}
+          />
         </div>
         <div>
-          <label htmlFor="price">Price</label>
-          <input type="number" id="price" ref={priceEl}></input>
+          <TextField
+            type="number"
+            required
+            id="price"
+            label="Price"
+            title="price"
+            onKeyDown={blockInvalidChar}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.price}
+            error={formik.touched.price && Boolean(formik.errors.price)}
+            helperText={formik.touched.price && formik.errors.price}
+          />
         </div>
         <div>
-          <label htmlFor="date">Date</label>
-          <input type="date" id="date" ref={dateEl}></input>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              label="Date"
+              title="date"
+              id="date"
+              required
+              disablePast
+              onChange={(date) => formik.setFieldValue('date', date)}
+              onBlur={() => formik.setFieldTouched('date', true)}
+              value={formik.values.date}
+              slotProps={{
+                textField: {
+                  required: true,
+                  error: formik.touched.date && Boolean(formik.errors.date),
+                  helperText: formik.touched.date ? formik.errors.date : '',
+                },
+              }}
+            />
+          </LocalizationProvider>
         </div>
         <div>
-          <button type="submit">Submit</button>
+          <Button type="submit" variant="contained">
+            Submit
+          </Button>
         </div>
-      </form>
+      </Box>
     </React.Fragment>
   );
 }
